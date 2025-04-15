6118213a5e36b23e9708482ba57baae2ded0712e
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2025-04-15 18:28:01 +0300

    Revunkov lab5: functors and monadas

--- Revunkov/Program.fs
+++ Revunkov/Program.fs
@@ -3,6 +3,7 @@ open FigureDU
 open Agent
 open System
 open Agent.Agent
+open FunctorsAndMonadas
 
 [<EntryPoint>]
 let main argv =
@@ -37,4 +38,28 @@ let main argv =
     agent.Post Stop
 
     System.Threading.Thread.Sleep(500)
+
+    let testCases = [Just 5; Nothing]
+    
+    Console.WriteLine("Проверка законов функтора:")
+    testCases |> List.iter (fun x ->
+        Console.WriteLine($"Тождественный закон для {x}: {Functor.Laws.identity x}")
+        Console.WriteLine($"Закон композиции для {x}: {Functor.Laws.composition ((+) 2) ((*) 3) x}")
+    )
+
+    Console.WriteLine("\nПроверка законов аппликативного функтора:")
+    testCases |> List.iter (fun v ->
+        Console.WriteLine($"Тождественный закон для {v}: {Applicative.Laws.identity v}")
+    )
+    Console.WriteLine($"Гомоморфизм: {Applicative.Laws.homomorphism ((+) 2) 5}")
+    Console.WriteLine($"Закон перестановки: {Applicative.Laws.interchange (Just ((+) 2)) 5}")
+
+    Console.WriteLine("\nПроверка законов монады:")
+    let f x = Just (x + 2)
+    let g x = Just (x * 3)
+    testCases |> List.iter (fun m ->
+        Console.WriteLine($"Левый нейтральный элемент (левая единица): {Monad.Laws.leftIdentity 5 f}")
+        Console.WriteLine($"Правый нейтральный элемент (правая единица) для {m}: {Monad.Laws.rightIdentity m}")
+        Console.WriteLine($"Ассоциативность для {m}: {Monad.Laws.associativity m f g}")
+    )
     0
