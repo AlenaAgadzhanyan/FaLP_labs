39f792cc8cfc0b51b088014a6c14f5df867d98b7
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2025-05-02 21:43:28 +0300

    lab 9 task 3: сначала положительные, потом отрицательные элементы

--- Prolog/RecursionsAndLists/lists.pl
+++ Prolog/RecursionsAndLists/lists.pl
@@ -1,4 +1,4 @@
-# 1.10	Даны два массива. Необходимо найти количество совпадающих по значению элементов.
+% 1.10	Даны два массива. Необходимо найти количество совпадающих по значению элементов.
 % count_matches(+List1, +List2, -Count)
 
 count_matches([], _, 0) :- !.
@@ -11,3 +11,22 @@ count_matches([H|T], List2, Count) :-
 
 count_matches([_|T], List2, Count) :-
     count_matches(T, List2, Count).
+
+
+% 1.46	Дан целочисленный массив. Необходимо вывести вначале его положительные элементы, а затем - отрицательные.
+
+% positives_then_negatives(+List, -Result)
+
+separate_positives_and_negatives([], [], []) :- !.
+
+separate_positives_and_negatives([H|T], [H|Positives], Negatives) :-
+    H > 0,
+    separate_positives_and_negatives(T, Positives, Negatives).
+
+separate_positives_and_negatives([H|T], Positives, [H|Negatives]) :-
+    H =< 0, 
+    separate_positives_and_negatives(T, Positives, Negatives).
+
+positives_then_negatives(List, Result) :-
+    separate_positives_and_negatives(List, Positives, Negatives),
+    append(Positives, Negatives, Result).
